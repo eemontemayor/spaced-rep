5e50dc9c7717a57c97ee9bb6511cb014d666de70
Enrique Montemayor <eemontemayor@live.com>
2020-01-24 18:45:37 -0600

    feedback issue fixed

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import LangContext from '../../contexts/LangContext'
 import LangService from '../../services/lang-service'
 import Button from '../../components/Button/Button'
-import { Label, Input } from '../../components/Form/Form'
-// import './LearningRoute.css'
+import { Label,Textarea , Input} from '../../components/Form/Form'
 class LearningRoute extends Component {
   state= {
     guess_input: '',
@@ -43,8 +42,8 @@ class LearningRoute extends Component {
     //keeps track of word user just finished submitting
     this.setState({
       prevWord: this.state.nextWord,
-      // prevIncorrectCount: this.state.wordIncorrectCount,
-      // prevCorrectCount:this.state.wordCorrectCount
+      prevIncorrectCount: this.state.wordIncorrectCount,
+      prevCorrectCount:this.state.wordCorrectCount
     }, () => {
        
     LangService.postGuess(guess)
@@ -71,13 +70,16 @@ class LearningRoute extends Component {
         let message = null;
         //when the user has not entered anything
         let displayForm = null;
-    
+        let wordData = null
         if(this.state.isCorrect) {
           message = <><h2>You were correct! :D</h2></>;
           displayForm = <><Button type='click' onClick={this.handleNext.bind(this)}>Try another word!</Button></>
+          wordData =<><p>{`You have answered this word correctly ${this.state.prevCorrectCount+1} times.`}</p>
+          <p>{`You have answered this word incorrectly ${this.state.prevIncorrectCount} times.`}</p></>
         } else {
           message = <><h2>Good try, but not quite right :(</h2><p>{`The correct translation for ${this.state.prevWord} was ${this.state.answer} and you chose ${this.state.guess_input}!`}</p></>;
-          
+          wordData =<><p>{`You have answered this word correctly ${this.state.prevCorrectCount} times.`}</p>
+          <p>{`You have answered this word incorrectly ${this.state.prevIncorrectCount +1} times.`}</p></>
           //if the user entered the wrong word
           displayForm = <><Button type='click' onClick={this.handleNext.bind(this)}>Try another word!</Button></>
         } 
@@ -91,6 +93,8 @@ class LearningRoute extends Component {
               <Input type='text' id='learn-guess-input' name='guess_input' onChange={this.handleChange.bind(this)} required />
               <Button type='submit'>Submit your answer</Button>
             </form></>
+             wordData =<><p>{`You have answered this word correctly ${this.state.wordCorrectCount} times.`}</p>
+             <p>{`You have answered this word incorrectly ${this.state.wordIncorrectCount} times.`}</p></>
         }
     
    
@@ -104,11 +108,10 @@ class LearningRoute extends Component {
           {displayForm}      
           <div className="DisplayScore">
             <p>{`Your total score is: ${this.state.totalScore}`}</p>
-          </div>
-          <div className= "WordData">
-              <p>{`You have answered this word correctly ${this.state.wordCorrectCount} times.`}</p>
-              <p>{`You have answered this word incorrectly ${this.state.wordIncorrectCount} times.`}</p>
-          </div>
+            </div>
+            <div className='DisplayWordData'>
+              {wordData}
+            </div>
         </main>
          </div>
       );
