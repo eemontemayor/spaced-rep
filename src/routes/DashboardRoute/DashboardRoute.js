e5587dbd8a76ef6d283536c5afba0124ed8f3a32
Enrique Montemayor <eemontemayor@live.com>
2020-01-30 14:58:38 -0600

    cleaned up code

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,127 +1,84 @@
-import React, { Component } from 'react'
-import LangService from '../../services/lang-service'
-import { Link } from 'react-router-dom';
-import Word from '../../components/Word/Word'
-import TotalScore from '../../components/TotalScore/TotalScore'
-import LangContext from '../../contexts/LangContext';
-import './DashboardRoute.css'
+import React, { Component } from "react";
+import LangService from "../../services/lang-service";
+import { Link } from "react-router-dom";
+import Word from "../../components/Word/Word";
+import TotalScore from "../../components/TotalScore/TotalScore";
+import LangContext from "../../contexts/LangContext";
+import "./DashboardRoute.css";
 
 class DashboardRoute extends Component {
   state = {
-    languageId:null,
-    language:'',
+    languageId: null,
+    language: "",
     words: [],
-    nextWordId:null,
-    totalScore:null,
-
-  }
-  
-
+    nextWordId: null,
+    totalScore: null
+  };
 
   componentDidMount() {
-
-    LangService.getUserLanguage()
-    .then(res => {
+    LangService.getUserLanguage().then(res => {
       console.log(res);
       this.setState({
-        languageId:res.language.id,
-        language:res.language.name,
+        languageId: res.language.id,
+        language: res.language.name,
         totalScore: res.language.total_score,
-        // nextWordId: res.language.head,
-        words:[...res.words]
-      }, () => {
-          
-        console.log(this.state);
-      })
-
-    })
+        words: [...res.words]
+      });
+    });
   }
 
-
-//    TODO = 
-// REFACTOR STUPID QUESTION AND FEEDBACK SECTION
-// FINISH STYLING 
-        //GIVE START PRACTICING BUTTON SOME COOL FEATURE
-// CHANGE DB TO SPANISH 
-// ADD A FOOTER THAT EXPLAINS THE ALGO --> LANDING PAGE PRESENTS THE DETAILS
-// DEPLOY  
-// INCLUDE PHOENETIC PRONUNCIATION
-// ALLOW USER TO SWITCH LANGUAGES X TO ENGLISH, ENGLISH TO X SO THEY CAN GUESS BIDIRECTIONALLY AND SPRINKLE A FLIPPED ONE IN THERE RANDOMLY
-//****NEW ROUTE: WHEN USER CLICKS ON TOTAL SCORE IT WILL TAKE THEM TO A TRENDS + DATA PAGE
-//***** NEW ROUTE: ADD A BUTTON ON TOP OF LIST TO ALLOW USER TO ADD A NEW WORD
-// WHEN USER CLICKS ON WORD CARD FROM DASHBOARD ROUTE CARD WILL FLIP TO SHOW A PICTURE
-// ALLOW USER TO SHOW PW ON LOGIN FORM
-//ADD MANDARIN
-  generateWordList=(words)=>{
-    
-      let wordList= words.map((item,index)=>{
-        return <li key={index}>
+  generateWordList = words => {
+    let wordList = words.map((item, index) => {
+      return (
+        <li key={index}>
           <Word
             original={item.original}
             correct_count={item.correct_count}
             incorrect_count={item.incorrect_count}
           />
         </li>
-      })
-    return wordList
-  }
+      );
+    });
+    return wordList;
+  };
 
   render() {
-
-   
-
-
-    let wordList= this.generateWordList(this.state.words);
-   
+    let wordList = this.generateWordList(this.state.words);
 
     const value = {
-      languageId:this.state.languageId,
-      language:this.state.language,    
-      words:this.state.words,               
-      totalScore:this.state.totalScore
-    }
-
-
+      languageId: this.state.languageId,
+      language: this.state.language,
+      words: this.state.words,
+      totalScore: this.state.totalScore
+    };
 
     return (
       <LangContext.Provider value={value} role="main">
-      <main> 
-        <div className='DashboardPage'> 
-            
-              <div className='NameContainer'>
-
-            <h2 className='lang-name'>{this.state.language}</h2>
-              </div>
-            <div className='ScoreContainer'>
-
-            <TotalScore score={this.state.totalScore} />         
+        <main>
+          <div className="DashboardPage">
+            <div className="NameContainer">
+              <h2 className="lang-name">{this.state.language}</h2>
             </div>
-      
-              
-            <button className='StartBtnContainer start-pract-btn'>
-                <Link to={`/learn`}>
-                  <p>
-                  Start practicing
-                </p>
-                  </Link>
+            <div className="ScoreContainer">
+              <TotalScore score={this.state.totalScore} />
+            </div>
+
+            <button className="StartBtnContainer start-pract-btn">
+              <Link to={`/learn`}>
+                <p>Start practicing</p>
+              </Link>
             </button>
-       
-           
-            <div className='ListContainer'>
-            <h3 className='list-msg'>
-              Words to practice
-            </h3>
-              
-            <ul className='word-list'>
-                {wordList}
-            </ul>
-           
+
+            <div className="ListContainer">
+              <h3 className="list-msg">Words to practice</h3>
+
+              <ul className="word-list">{wordList}</ul>
+            </div>
           </div>
-        </div>
-      </main>
-     </LangContext.Provider>
+        </main>
+      </LangContext.Provider>
     );
   }
 }
 
-export default DashboardRoute
+export default DashboardRoute;
--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,126 +1,178 @@
-import React, { Component } from 'react'
-import LangContext from '../../contexts/LangContext'
-import LangService from '../../services/lang-service'
-import Button from '../../components/Button/Button'
-import { Label, Input } from '../../components/Form/Form'
-import './LearningRoute.css'
+import React, { Component } from "react";
+import LangContext from "../../contexts/LangContext";
+import LangService from "../../services/lang-service";
+import Button from "../../components/Button/Button";
+import { Label, Input } from "../../components/Form/Form";
+import "./LearningRoute.css";
 class LearningRoute extends Component {
-  state= {
-    guess_input: '',
-    prevWord:'',
-    nextWord: '',
-    answer:null,
-    // wordCorrectCount: null,
-    // wordIncorrectCount: null,
-    // isCorrect:false,
-    // total_score:8,
-  }
-  static contextType = LangContext
-  GuessInput = React.createRef()
+  state = {
+    guess_input: "",
+    prevWord: "",
+    nextWord: "",
+    answer: null
+  };
+  static contextType = LangContext;
+  GuessInput = React.createRef();
 
   componentDidMount() {
-    
-    LangService.getHeadWord()
-    .then(res => {
-      console.log(res)
-      for(const [key,value] of Object.entries(res)){
+    LangService.getHeadWord().then(res => {
+      console.log(res);
+      for (const [key, value] of Object.entries(res)) {
         this.setState({
-          [key]:value,
-        })
+          [key]: value
+        });
       }
-    })
-    this.GuessInput.current.focus()
+    });
+    this.GuessInput.current.focus();
   }
 
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
     });
-  }
+  };
 
-  handleSubmit = (e) =>{
-    e.preventDefault()
-    const guess = this.state.guess_input.toLowerCase()
-    console.log(guess)
+  handleSubmit = e => {
+    e.preventDefault();
+    const guess = this.state.guess_input.toLowerCase();
+    console.log(guess);
     //keeps track of word user just finished submitting
-    this.setState({
-      prevWord: this.state.nextWord,
-      prevIncorrectCount: this.state.wordIncorrectCount,
-      prevCorrectCount:this.state.wordCorrectCount
-    }, () => {
-       
-    LangService.postGuess(guess)
-    .then(res => {
-      for(const [key,value] of Object.entries(res)){
-        this.setState({
-          [key]:value,
-        })
+    this.setState(
+      {
+        prevWord: this.state.nextWord,
+        prevIncorrectCount: this.state.wordIncorrectCount,
+        prevCorrectCount: this.state.wordCorrectCount
+      },
+      () => {
+        LangService.postGuess(guess).then(res => {
+          for (const [key, value] of Object.entries(res)) {
+            this.setState({
+              [key]: value
+            });
+          }
+        });
       }
-    }) 
-    });
+    );
+  };
 
-  }
-
-  handleNext=(e)=>{
-    e.preventDefault()
+  handleNext = e => {
+    e.preventDefault();
     this.setState({
       ...this.state,
-      answer: null,
-    })
-  }
+      answer: null
+    });
+  };
   render() {
-        //when there is an answer
-        let message = null;
-        //when the user has not entered anything
-        let displayForm = null;
-        let wordData = null
-        if(this.state.isCorrect) {
-          message = <><h2 className='feedback-message'>You were correct! :D</h2></>;
-          displayForm = <><Button type='click' className='NextWordBtn' onClick={this.handleNext.bind(this)}>Try another word!</Button></>
-          wordData =<><p className='WordData green'>{`correct guesses: ${this.state.prevCorrectCount+1}`}</p>
-          <p className='WordData'>{`incorrect guesses: ${this.state.prevIncorrectCount}`}</p></>
-        } else {
-          message = <><h2 className='feedback-message'>Good try, but not quite right :(</h2><p>{`The correct translation for ${this.state.prevWord} was ${this.state.answer} and you chose ${this.state.guess_input}!`}</p></>;
-          wordData =<><p className='WordData'>{`correct guesses: ${this.state.prevCorrectCount}`}</p>
-          <p className='WordData red'>{`incorrect guesses: ${this.state.prevIncorrectCount +1}`}</p></>
-          //if the user entered the wrong word
-          displayForm = <><Button type='click' className='NextWordBtn'onClick={this.handleNext.bind(this)}>Try another word!</Button></>
-        } 
-    
-        //for if there is no answer
-        if (!this.state.answer) {
-          message = <><h2>Translate the word:</h2> <span id='quest-word'>{this.state.nextWord}</span></>;
-          displayForm = <>
-            <form id='quest-form' onSubmit={this.handleSubmit.bind(this)}>
-              <Label htmlFor='learn-guess-input' className='quest-text'>What's the translation for this word?</Label><br/>
-              <Input ref={this.GuessInput}type='text' id='learn-guess-input' name='guess_input' onChange={this.handleChange.bind(this)} required />
-              <Button className='guess-submit-btn'type='submit'>Submit your answer</Button>
-            </form></>
-             wordData =<><p className='WordData'>{`incorrect guesses: ${this.state.wordCorrectCount}`}</p>
-             <p className='WordData'>{`correct guesses: ${this.state.wordIncorrectCount}`}</p></>
-        }
-    
-   
-    
-      return (
-        <div role="main" className='question-card'>
-        <main className="score-form">
-          <div className="DisplayFeedback">
-          {message}
-          </div>
-          {displayForm}      
-          <div className="DisplayScore">
-            <p className='total-score-quest-fb'>{`Total Score: ${this.state.totalScore}`}</p>
-            </div>
-            <div className='DisplayWordData'>
-              WORD STATS:
-              {wordData}
-            </div>
-          </main>
-      
-         </div>
+    //when there is an answer
+    let message = null;
+    //when the user has not entered anything
+    let displayForm = null;
+    let wordData = null;
+    if (this.state.isCorrect) {
+      message = (
+        <>
+          <h2 className="feedback-message">You were correct! :D</h2>
+        </>
+      );
+      displayForm = (
+        <>
+          <Button
+            type="click"
+            className="NextWordBtn"
+            onClick={this.handleNext.bind(this)}
+          >
+            Try another word!
+          </Button>
+        </>
+      );
+      wordData = (
+        <>
+          <p className="WordData green">{`correct guesses: ${this.state
+            .prevCorrectCount + 1}`}</p>
+          <p className="WordData">{`incorrect guesses: ${this.state.prevIncorrectCount}`}</p>
+        </>
+      );
+    } else {
+      message = (
+        <>
+          <h2 className="feedback-message">Good try, but not quite right :(</h2>
+          <p>{`The correct translation for ${this.state.prevWord} was ${this.state.answer} and you chose ${this.state.guess_input}!`}</p>
+        </>
+      );
+      wordData = (
+        <>
+          <p className="WordData">{`correct guesses: ${this.state.prevCorrectCount}`}</p>
+          <p className="WordData red">{`incorrect guesses: ${this.state
+            .prevIncorrectCount + 1}`}</p>
+        </>
+      );
+      //if the user entered the wrong word
+      displayForm = (
+        <>
+          <Button
+            type="click"
+            className="NextWordBtn"
+            onClick={this.handleNext.bind(this)}
+          >
+            Try another word!
+          </Button>
+        </>
       );
     }
+
+    //for if there is no answer
+    if (!this.state.answer) {
+      message = (
+        <>
+          <h2>Translate the word:</h2>{" "}
+          <span id="quest-word">{this.state.nextWord}</span>
+        </>
+      );
+      displayForm = (
+        <>
+          <form id="quest-form" onSubmit={this.handleSubmit.bind(this)}>
+            <Label htmlFor="learn-guess-input" className="quest-text">
+              What's the translation for this word?
+            </Label>
+            <br />
+            <Input
+              ref={this.GuessInput}
+              type="text"
+              id="learn-guess-input"
+              name="guess_input"
+              onChange={this.handleChange.bind(this)}
+              required
+            />
+            <Button className="guess-submit-btn" type="submit">
+              Submit your answer
+            </Button>
+          </form>
+        </>
+      );
+      wordData = (
+        <>
+          <p className="WordData">{`incorrect guesses: ${this.state.wordCorrectCount}`}</p>
+          <p className="WordData">{`correct guesses: ${this.state.wordIncorrectCount}`}</p>
+        </>
+      );
+    }
+
+    return (
+      <div role="main" className="question-card">
+        <main className="score-form">
+          <div className="DisplayFeedback">{message}</div>
+          {displayForm}
+          <div className="DisplayScore">
+            <p className="total-score-quest-fb">{`Total Score: ${this.state.totalScore}`}</p>
+          </div>
+          <div className="DisplayWordData">
+            WORD STATS:
+            {wordData}
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
-export default LearningRoute
+export default LearningRoute;
