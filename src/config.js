06e6076d19b2c0e418816be605c49aa7eafec761
Enrique Montemayor <eemontemayor@live.com>
2020-01-30 17:48:32 -0600

    x

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+  API_ENDPOINT:'https://limitless-eyrie-48946.herokuapp.com/api',
+  // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
   // API_ENDPOINT: 'http://localhost:8000/api', 
   TOKEN_KEY:process.env.REACT_APP_TOKEN_KEY
 }
