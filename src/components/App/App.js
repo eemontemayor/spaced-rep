3321fabc5500f2fe80c4ec60205c3781a472b047
Enrique Montemayor <eemontemayor@live.com>
2020-02-17 11:53:46 -0600

    x

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -36,16 +36,21 @@ export default class App extends Component {
     });
   }
 
-
+//===========================================
 
 //TO-DO
-  // deleteWord = add a pop up card when delete is successful to notify user
+  // deleteWord 
     //            = think bout how total score is affected or should be affected
   
+  // make a landing page that explains... and add a trouble-free demo button
+
+  // re - seed database so it can handle words with accents
+  //------------------------- deploy ------
   //add Word = add validation func
               // =  search through snip its stuff or some other dictionary api
               // = make sure adding words with accents is not a problem
-
+           //   add a pop up card when delete or post is successful to notify user
+//===========================================
 
 
   render() {
