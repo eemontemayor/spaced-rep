62650bcf20aa44166c1070cb8506634d32dfea64
Enrique Montemayor <eemontemayor@live.com>
2020-03-10 13:57:08 -0500

    demo almost done

--- src/routes/DemoRoute/DemoRoute.js
+++ src/routes/DemoRoute/DemoRoute.js
@@ -1,12 +1,216 @@
 import React from "react";
 import "./DemoRoute.css";
-export default class DemoRoute extends React.Component {
+import Word from '../../components/Word/Word'
+import Button from "../../components/Button/Button";
+import { Label, Input } from "../../components/Form/Form";
 
-  render() {
-      return (
-          <>
+import Tooltip from '../../components/Tooltip/Tooltip'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+export default class DemoRoute extends React.Component {
+    state = {
+        words: [],
+        guess_input:'',
+        headWord: '',
+        translation: '',
+        counter: 0,
+      };
+  GuessInput = React.createRef();
+    
+
+    componentDidMount() {
+        const demoWords= [
+            {
+              id: 1,
+              original: "perro",
+              translation: "dog",
+              correct_count: 0,
+              incorrect_count: 0
+            },
+            {
+              id: 2,
+              original: "gato",
+              translation: "cat",
+              correct_count: 0,
+              incorrect_count: 0
+            },
+            {
+              id: 3,
+              original: "mano",
+              translation: "hand",
+              correct_count: 0,
+              incorrect_count: 0
+            },
+            {
+              id: 4,
+              original: "elefante",
+              translation: "elephant",
+              correct_count: 0,
+              incorrect_count: 0
+            },
+            {
+                id: 5,
+                original: 'casa',
+                translation: 'home',
+                correct_count: 0,
+                incorrect_count:0
+            },
+            {
+                id: 6,
+                original: 'sol',
+                translation: 'sun',
+                correct_count: 0,
+                incorrect_count:0
+            },
+            
+        
+        ]
+        
+    
+        this.setState({
+            words: demoWords,
+        
+        }, () => {
+            for (const [key, value] of Object.entries(demoWords[0])) {
+                this.setState({
+                    [key]: value
+                });
+            }
+        })
+        this.GuessInput.current.focus();
+    }
+
+    generateDemoList = words => {
+        let wordList = words.map((item, index) => {
+          return (
+            <li key={index}>
+         
               
-          </>
-      )
+              <Word
+                id={item.id}
+                original={item.original}
+                correct_count={item.correct_count}
+                incorrect_count={item.incorrect_count}
+                  />
+                    
+    
+               
+            </li>
+          );
+        });
+        return wordList;
+      };
+    handleChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        }, () => {
+                console.log(this.state)
+        });
+      };
+    handleNext = e => {
+        e.preventDefault();
+        this.setState({
+          ...this.state,
+          answer: null
+        });
+      };
+      handleSubmit = e => {
+          e.preventDefault();
+          let wordList = this.state.words
+          let headWord = this.state.headWord
+          let counter = this.state.counter
+          const guess = this.state.guess_input.toLowerCase();
+     console.log(wordList)
+          if (headWord === guess && counter===0) {
+              
+              alert('When you guess the word correctly, the word will be inserted further down the list')
+     
+          }
+          if (headWord === guess && counter === 1) {
+              alert('Your progress is saved on the back end, using a Linked List')
+            
+          }
+          if (headWord === guess && counter === 2) {
+              alert('Almost Done')
+  
+           }
+          if (headWord === guess && counter === 3) {
+            alert('The more consecutive correct guesses for a given word will push that word even further down so you will see it less often')
+          
+       
+          }
+          if (headWord !== guess && counter === 3) {
+            alert('But beware: if you answer a word incorrectly, you will see the word again soon')
+          
+       
+          }
+          let oldHeadWord = wordList.shift()
+  
+          oldHeadWord.correct_count += 1
+          let index = oldHeadWord.correct_count * 2
+          wordList.splice(index,0,oldHeadWord)
+          console.log(wordList) 
+          this.setState({
+              words: wordList,
+              counter:this.state.counter + 1
+              
+          }, () => {
+            for (const [key, value] of Object.entries(wordList[0])) {
+                this.setState({
+                    [key]: value
+                });
+            }
+          })
+      };
+    render() {
+        let wordList = this.generateDemoList(this.state.words)
+        let headWord = this.state.original
+        let translation = this.state.translation
+        let counter = this.state.counter
+        // console.log(this.state.headWord)
+        return (
+            <div className='DemoPage'>
+    <div className='question-card'>
+                <>
+          <h2>Translate the word:</h2>{" "}
+                        <span id="quest-word">
+                            {headWord}
+                        </span>
+        </>
+                <form id="quest-form"  onSubmit={this.handleSubmit}>
+            <Label htmlFor="learn-guess-input" className="quest-text">
+              What's the translation for this word?
+            </Label>
+            <br />
+                        <Input
+                         
+                            ref={this.GuessInput}
+                            type="text"
+                            id="learn-guess-input"
+                            name="guess_input"
+                            value={counter !== 4 ? translation : 'malarkey'}
+                            disabled
+              onChange={this.handleChange.bind(this)}
+              required
+                        />
+                        <Button className="guess-submit-btn" type='submit' >
+                       
+              Submit your answer
+            </Button>
+                        
+          </form>
+                </div>
+            <div className="ListContainer">
+    <h3 className="list-msg">DEMO Word List</h3>
+      <ul className="word-list">
+        {wordList}
+  
+      </ul>
+                </div>
+            
+            </div>
+    
+    )
   }
 }
